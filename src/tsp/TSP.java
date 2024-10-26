222d55d7f006dd42c21db2d626ebe863ff282834
javie <javie@Acer-Javier>
2024-10-26 18:02:47 +0200

    hola

---
+++ src/tsp/TSP.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tsp;
+
+/**
+ *
+ * @author elena
+ */
+public class TSP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //prueba neat beans
+    }
+    
+}
