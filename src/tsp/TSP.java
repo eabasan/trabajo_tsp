bf0f42f35ec9371db5f30d501b44c078fe89f556
eabasan <elenaabato2@gmail.com>
2024-10-26 17:55:58 +0200

    carga inicial

--- src/tsp/TSP.java
+++ src/tsp/TSP.java
@@ -14,7 +14,7 @@ public class TSP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //prueba neat beans
     }
     
 }
